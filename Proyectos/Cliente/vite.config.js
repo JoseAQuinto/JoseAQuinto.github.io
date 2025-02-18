9537865de00d9893a7a9cbdc087bd187b3854984
Jose Angel <jose.angel.quinto.ferrandez@gmail.com>
2025-02-18 02:54:06 +0100

    cambio index principal

--- Proyectos/Cliente/vite.config.js
+++ Proyectos/Cliente/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/JoseAQuinto.github.io/", // Cambia esto al nombre de tu repo en GitHub
+  base: "/",
 });
